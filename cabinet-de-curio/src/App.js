2dc1ee3cf8684906b4ae47d413ef7300013cd7e8
William Berry <wberry264@gmail.com>
2023-02-16 18:59:38 -0500

    added React files

---
+++ cabinet-de-curio/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+        return (
+        <>
+            <header className='header'>
+                <nav className='navbar'>
+                    <img src='./logo.svg'>
+                    <h1>Header</h1>
+                </nav>
+            </header>
+        </>
+        )
+}
+
+export default App;
---
+++ cabinet-de-curio/src/Nav.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Nav() {
+    return (
+    <>
+        <header>
+            <nav>
+                <img src='./logo.svg'></img>
+                <h1>Header</h1>
+            </nav>
+        </header>
+    </>
+    )
+}
+
+export default Nav;
